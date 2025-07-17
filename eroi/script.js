ba313048abc6067452a135dfdf541689a636d35d
AlissCava <cavalleraalissa@gmail.com>
2025-07-17 19:02:59 +0200

    quai finito, mamca solo il link della quinta card

--- eroi/script.js
+++ eroi/script.js
@@ -1,11 +1,9 @@
 // script.js
 
-// Al passaggio del mouse sulla card, aggiunge la classe is-flipped per far girare la card
+// Al click del mouse sulla card, aggiunge la classe is-flipped per far girare la card
 document.querySelectorAll('.dog-card').forEach(card => {
-    card.addEventListener('mouseenter', () => {
-        card.querySelector('.dog-card-inner').classList.add('is-flipped');
+    card.addEventListener('click', () => {
+      const inner = card.querySelector('.dog-card-inner');
+      inner.classList.toggle('flipped');
     });
-    card.addEventListener('mouseleave', () => {
-        card.querySelector('.dog-card-inner').classList.remove('is-flipped');
-    });
-});
+  });
